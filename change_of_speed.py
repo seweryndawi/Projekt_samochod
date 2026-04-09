ed262d254d72311076c91b5df601200408001745
unknown <seweryndawi@gmail.com>
2026-04-09 14:15:11 +0200

    Zmiana stalej na zmienna

--- change_of_speed.py
+++ change_of_speed.py
@@ -8,7 +8,7 @@ def vel_change(df, veh_ID, frame1, rate_of_change):
         frame2 = frame1 + rate_of_change
         row1 = df[(df['Vehicle_ID'] == veh_ID) & (df['Frame_ID'] == frame1)].iloc[0]
         row2 = df[(df['Vehicle_ID'] == veh_ID) & (df['Frame_ID'] == frame2)].iloc[0]
-        change_of_velocity = (row2['v_Vel'] - row1['v_Vel']) / 10
+        change_of_velocity = (row2['v_Vel'] - row1['v_Vel']) / rate_of_change
     except:
         change_of_velocity = "Frame 2 out of bounds"
     return change_of_velocity
