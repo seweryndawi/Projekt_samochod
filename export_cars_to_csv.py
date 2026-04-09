ef1208585e37a7987dfbc51faa780349eccfe39c
unknown <seweryndawi@gmail.com>
2026-04-09 13:23:11 +0200

    Poprawa struktury pliku

--- export_cars_to_csv.py
+++ export_cars_to_csv.py
@@ -1,7 +1,7 @@
-def export_cars(df, cols, *args):
+def export_cars(df, cols, IDs):
     """Funkcja przyjmuje jako wartości ID samochodów. Na tej podstawie zapisuje dane dotyczące tego samochodu do pliku .csv
     Zmienna 'df' określa tablicę, z której chcemy eksportować dane.
-    Zmienna '*args' przyjmuje wartości ID samochodów, których dane chcemy eksportować.
+    Zmienna 'IDs' przyjmuje wartości ID samochodów, których dane chcemy eksportować.
     Zmienna 'cols' przyjmuje kolumny (w formacie 'str'), które chcemy użyć.
     """
-    df[df['Vehicle_ID'].isin(*args)].to_csv("filtered.csv", columns = cols, index = False)
+    df[df['Vehicle_ID'].isin(IDs)].to_csv("filtered.csv", columns = cols, index = False)
