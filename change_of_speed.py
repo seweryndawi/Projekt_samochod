dfacb3d3c1bf74adcfef325ec10dd6634a3bc495
unknown <seweryndawi@gmail.com>
2026-04-09 13:47:40 +0200

    Poprawki czytelnosci

--- change_of_speed.py
+++ change_of_speed.py
@@ -1,17 +1,14 @@
-import numpy as np
-import pandas as pd
-
-def change(df, veh_ID, frame1, rate_of_change):
+def vel_change(df, veh_ID, frame1, rate_of_change):
     """ Funkcja pozwalająca na wyznaczenie zmiany prędkości pomiędzy dwoma okresami czasu.
     Zmienna 'frame1' przyjmuje jako wartość klatkę z początkiem ruchu.
-    Zmienna 'rate_of_change' określa długość ruchu. Wartość 1 klatka = 100 ms.
+    Zmienna 'rate_of_change' określa długość ruchu, przy założeniu, że 1 klatka = 100 ms.
     Ta funkcja używa pierwszego wzoru z kartki.
     """
     try:
         frame2 = frame1 + rate_of_change
         row1 = df[(df['Vehicle_ID'] == veh_ID) & (df['Frame_ID'] == frame1)].iloc[0]
         row2 = df[(df['Vehicle_ID'] == veh_ID) & (df['Frame_ID'] == frame2)].iloc[0]
-        change_of_speed = (row2['v_Vel'] - row1['v_Vel']) / 10
+        change_of_velocity = (row2['v_Vel'] - row1['v_Vel']) / 10
     except:
-        change_of_speed = "Frame 2 out of bounds"
-    return change_of_speed
+        change_of_velocity = "Frame 2 out of bounds"
+    return change_of_velocity
--- find_v0.py
+++ find_v0.py
@@ -1,4 +1,3 @@
-import pandas as pd
 def find_v0(df, veh_ID):
     row = df[df['Vehicle_ID'] == veh_ID].iloc[0]
     return row["v_Vel"]
